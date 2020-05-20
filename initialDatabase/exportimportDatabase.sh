a007c1e543aa80cd2a16f873f8ce9ec2a9c40d6f
Tai WU <tai.wu@sap.com>
2020-05-20 16:39:11 +0800

    add for dockerize

---
+++ initialDatabase/exportimportDatabase.sh
@@ -0,0 +1,12 @@
+mongoexport --db vuedashboardtiles -c burndowncharts -o burndowncharts_vuedashboardInitialDump.json
+mongoexport --db vuedashboardtiles -c burndownchartsprints -o burndownchartsprints_vuedashboardInitialDump.json
+mongoexport --db vuedashboardtiles -c burndownchartstories -o burndownchartstories_vuedashboardInitialDump.json
+mongoexport --db vuedashboardtiles -c dashboardconfigs -o dashboardconfigs_vuedashboardInitialDump.json
+mongoexport --db vuedashboardtiles -c newutcodecoverages -o newutcodecoverages_vuedashboardInitialDump.json
+
+
+mongoimport --port 37117 --db vuedashboardtiles -c burndowncharts --file burndowncharts_vuedashboardInitialDump.json
+mongoimport --port 37117 --db vuedashboardtiles -c burndownchartsprints --file burndownchartsprints_vuedashboardInitialDump.json
+mongoimport --port 37117 --db vuedashboardtiles -c dashboardconfigs --file burndownchartstories_vuedashboardInitialDump.json
+mongoimport --port 37117 --db vuedashboardtiles -c sprintdays --file dashboardconfigs_vuedashboardInitialDump.json
+mongoimport --port 37117 --db vuedashboardtiles -c newutcodecoverages --file newutcodecoverages_vuedashboardInitialDump.json
